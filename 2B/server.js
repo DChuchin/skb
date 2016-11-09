72aa046b63838d6cc26d7743082ea68cb1576317
DmitryChuchin <dime10cents@gmail.com>
2016-11-09 11:52:36 +0600

    split fullname

--- 2B/server.js
+++ 2B/server.js
@@ -5,10 +5,23 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res)=> {
-  const fullName= req.query.fullname;
-  res.status(200).send(fullName);
+  const fullName= req.query.fullname.split(' ').reverse();
+  const [lastName, secondName, firstName] = fullName;
+  let result;
+
+  if ((fullName.length > 3)||(!fullName.length)||(!lastName)) {
+    result = 'Invalid fullname';
+  } else {
+    result = lastName + ' ' + cutName(firstName) + cutName(secondName);
+  }
+  console.log(fullName);
+  console.log(result);
+  res.status(200).send(result.trim());
 });
 
+function cutName(str) {
+  return str ? str[0]+ '. ': '';
+}
 app.listen(3000, ()=> {
   console.log('listening port 3000');
 })
