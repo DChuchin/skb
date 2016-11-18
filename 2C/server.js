2232bd57310a4a9afcc23e77d00159ed29a349ee
dmitry <dime10cents@gmail.com>
2016-11-18 19:53:33 +0600

    all tests pass

--- 2C/server.js
+++ 2C/server.js
@@ -7,12 +7,18 @@ app.use(cors());
 
 app.get('/*', (req, res)=> {
   if (req.query.username){
-    let name = req.query.username.split('/').reverse()[0];
+    let name = parseName(req.query.username);
     let result = name[0]=='@' ? name : '@' + name;
     res.status(200).send(result);
   } else
   res.status(200).send('Invalid username');
 });
+
+function parseName(str) {
+  let domain = str.split('//').reverse()[0];
+  let arr = domain.split('/');
+  return arr[1] || arr[0];
+}
 
 app.listen(3000, ()=> {
   console.log('listening port 3000');
